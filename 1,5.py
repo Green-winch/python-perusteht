678a8cdda8e9c6f2398083d7d533172ba6ad1328
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-08 09:22:36 +0300

    Update 1,5.py

--- 1,5.py
+++ 1,5.py
@@ -1,3 +1,4 @@
+1.5 Luo muuttuja matka ja aseta sen arvoksi käyttäjän syöttämä arvo Luo muuttuja nopeus ja aseta sen arvoksi käyttäjän syöttämä arvo Laske ja printtaa matkustamiseen kulunut aika
 luku1 = input("Matkan pituus:")
 luku1 = int(luku1)
 luku2 = input("Ajonopeus:")
