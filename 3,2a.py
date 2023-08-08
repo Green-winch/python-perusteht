9726ae70ae6ede6739c4ff278e1446b94d1041c4
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-08 10:47:30 +0300

    Create 3,2a.py

---
+++ 3,2a.py
@@ -0,0 +1,5 @@
+#3.2a Luo muuttuja silakat, aseta sen arvoksi 10. Luo while toistolause, jonka ehtona on, että silakoita > 0. Joka toistolla printtaa silakoiden määrä ja vähennä silakoiden määrää yhdellä.
+silakat = 10
+while silakat > 0:
+    print("Silakoita:", silakat)
+    silakat -= 1
