e6276e1856e2f738a5c38da59672382e7392cb2f
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-08 12:24:01 +0300

    Create 4,1b.py

---
+++ 4,1b.py
@@ -0,0 +1,3 @@
+#4.1b Printtaa ja kutsu summa funktiota luvuilla 3 ja 5. Printtaa ja kutsu summa funktiota luvuilla 100 ja 83. Printtaa ja kutsu summa funktiota haluamasilla luvuilla.
+print(summa(3, 5))
+print(summa(100, 83))
