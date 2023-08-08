3e863bf237fc810181cd9080169a386ae67890b4
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-08 13:15:07 +0300

    Update 4,4b.py

--- 4,4b.py
+++ 4,4b.py
@@ -1,2 +1,5 @@
 #4.4b Luo funktio nimeltään Hajoa, joka lisää bugien määrää kertomalla kolmella ja printtaa bugien määrän. Funktio Korjaa tulee kutsua funktio hajoa.
-
+def Hajoa():
+    global bugit
+    bugit *= 3
+    print("Hajosi, bugien määrä:", bugit)
