2acb0373fa98865b7b37b69cdefaface2b8bf01d
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-08 12:10:29 +0300

    Create 3,4a.py

---
+++ 3,4a.py
@@ -0,0 +1,5 @@
+#3.4a Luo muuttuja nimeltään luku ja aseta sen arvoksi random luku väliltä 1 - 99. Luo muuttuja arvaus ja aseta sen arvoksi 0.
+import random
+
+luku = random.randint(1, 99)
+arvaus = 0
