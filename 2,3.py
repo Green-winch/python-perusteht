a73cd10dcf8d6d5ccadbef6b6980b35b7c29d1d4
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-08 09:28:47 +0300

    Update 2,3.py

--- 2,3.py
+++ 2,3.py
@@ -1,3 +1,5 @@
+#2.3 Luo muuttuja, jonka arvoksi asetat random luvun väliltä 0-20. Luo ehto, jos muuttuja on pienempi kuin 5, printtaa "Liian pieni". 
+#    Luo ehto, jos muuttuja on suurempi kuin 15, printtaa "Liian suuri". Luo ehto, jos muuttuja on väliltä 5-15, printtaa muuttuja.
 import random
 
 pkolikko = input("Kruunu vai klaava?: ")
