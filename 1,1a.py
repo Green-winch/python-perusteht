46737a0ef978fb29ef03208479f2a6e4bd3a923a
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-08 09:13:15 +0300

    Update 1,1a.py

--- 1,1a.py
+++ 1,1a.py
@@ -1,3 +1,3 @@
-#1.1a	Tee kaksi muuttujaa. Aseta näiden muuttujien arvoiksi itse valitsemasi luvut.
+#1.1a Tee kaksi muuttujaa. Aseta näiden muuttujien arvoiksi itse valitsemasi luvut.
 a = 84
 b = 48
