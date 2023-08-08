f804c07b97a32b24cf4b31103b37b10b144cac6e
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-08 10:55:33 +0300

    Create 3,3c.py

---
+++ 3,3c.py
@@ -0,0 +1,8 @@
+#3.3c While toistolauseen jälkeen printtaa Bensan määrä ja Euro määrä.
+Bensa = 0
+Euro = 0
+while Bensa < 50:
+    Bensa += 1
+    Euro += 1.80
+print("Bensan määrä:", Bensa)
+print("Euro määrä:", Euro)
