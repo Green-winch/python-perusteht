8c245c84cfc1b7329e3b4d4c841c36167e98b87d
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-08 10:54:26 +0300

    Create 3,3b.py

---
+++ 3,3b.py
@@ -0,0 +1,6 @@
+#3.3b Luo while toistolause, jonka ehtona on että, bensa on vähemmän kuin 50. Joka toistolla lisää bensan arvoa yhdellä ja Euron arvoa 1.80:llä
+Bensa = 0
+Euro = 0
+while Bensa < 50:
+    Bensa += 1
+    Euro += 1.80
