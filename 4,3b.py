ec204d6cb08f00d41298849fe563ea20d653fbde
Green-winch <118250794+Green-winch@users.noreply.github.com>
2023-08-08 12:35:15 +0300

    Create 4,3b.py

---
+++ 4,3b.py
@@ -0,0 +1,5 @@
+#4.3b Luo funktio nimeltään Lataa .Lataa funktio asettaa panoksien arvoksi 12:sta. printtaa: "Ladattu, panoksia 12"
+def Lataa():
+    global panokset
+    panokset = 12
+    print("Ladattu, panoksia", panokset)
